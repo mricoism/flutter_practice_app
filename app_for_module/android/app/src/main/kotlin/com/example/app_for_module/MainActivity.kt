69a686eb7cb9b332d231652cf0ff6232af6d8132
mricoism <mrikoismail73@gmail.com>
2024-12-12 22:17:07 +0700

    add project app_for_module, inside_shell_app_android_native, sample_app_one

---
+++ app_for_module/android/app/src/main/kotlin/com/example/app_for_module/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_for_module
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ inside_shell_app_android_native/android/app/src/main/kotlin/com/example/inside_shell_app_android_native/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.inside_shell_app_android_native
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
